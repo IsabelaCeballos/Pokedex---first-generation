c33bcd200af7be9afbb5ecef60366e9ab826e6c3
Isabela Ceballos <isaceballos2001@gmail.com>
2022-01-26 20:52:26 -0500

    detalles

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -60,7 +60,9 @@ export default function Content(){
     };   
     return(
         <>
-            <img className="img_title" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" alt="logo pokemon"/>
+            <header>
+                <img className="img_title" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png" alt="logo pokemon"/>
+            </header>
             <section className="cards">   
                 {  
                     pokemonsInfo ? renderizar() : <h2>Loading...</h2>   
